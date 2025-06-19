37cd329b7e4765a9598b424a80fa0b2048f267bd
Victor Nwakutere <nwakuterevictor1@gmail.com>
2025-06-19 09:16:06 -0700

    fix: allow user to give env path

--- package/protected_console_class.js
+++ package/protected_console_class.js
@@ -1,6 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+
 class ProtectedConsoleClass {
     constructor() {
+        const envPath = this._getEnvPathFromArgs();
+        dotenv.config({ path: envPath });
         this.forProd = (process.env.NODE_ENV === 'production')
+    }
+
+    _getEnvPathFromArgs() {
+        const envArgPrefix = '--env=';
+        const envArg = process.argv.find(arg => arg.startsWith(envArgPrefix));
+        if (envArg) {
+            const providedPath = envArg.slice(envArgPrefix.length);
+            return path.resolve(providedPath);
+        }
+        return path.resolve('.env');
     }
 
     logData(data, stillDisplayData = false) {
